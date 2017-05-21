df3d09e43893fcdfc41a5e495635666c9aba10dd
danny <danny75@gmail.com>
2017-05-21 17:07:17 +0800

    加入 CodeTemplates 控制器與檢視的程式碼範本

---
+++ MVC5Course/Models/ClientIndexVM.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC5Course.Models
+{
+    public class ClientIndexVM
+    {
+        public string Cred { get; set; }
+
+        public string LastNameList { get; set; }
+
+        public int pageNo { get; set; }
+    }
+}
